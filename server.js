395bc4b56b25340a0daf9cb6f7932d8d47aab66e
Alf Eaton <eaton.alf@gmail.com>
2019-04-25 10:47:37 +0100

    Remove hostname

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const app = require('./')
 
 const port = process.env.PORT || 8081
-const hostname = process.env.HOSTNAME || 'localhost'
+// const hostname = process.env.HOSTNAME || 'localhost'
 
-const server = app.listen(port, hostname, () => {
+// const server = app.listen(port, hostname, () => {
+const server = app.listen(port, () => {
   const { address, port } = server.address()
   console.log(`Listening at http://${address}:${port}/`)
 })

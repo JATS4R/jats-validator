48ed962d69071f263c240124f158ef896af306e7
Alf Eaton <eaton.alf@gmail.com>
2019-05-21 22:27:05 +0100

    Improve error handling

--- functions/format.js
+++ functions/format.js
@@ -3,16 +3,22 @@ const libxml = require('./libxml')
 module.exports = (req, res) => {
   const data = req.body.xml || req.file.buffer.toString()
 
-  const doc = libxml.parseXmlString(data, {
-    // dtdattr: true,
-    dtdload: true,
-    dtdvalid: true,
-    noblanks: true,
-    nocdata: true,
-    noent: true,
-    nonet: true,
-    nsclean: true,
-  })
+  try {
+    const doc = libxml.parseXmlString(data, {
+      // dtdattr: true,
+      dtdload: true,
+      dtdvalid: true,
+      noblanks: true,
+      nocdata: true,
+      noent: true,
+      nonet: true,
+      nsclean: true,
+    })
 
-  res.set('Content-Type', 'application/xml').send(doc.toString())
+    res.set('Content-Type', 'application/xml').send(doc.toString())
+  } catch (e) {
+    res.status(422).json({
+      error: e.message,
+    })
+  }
 }
